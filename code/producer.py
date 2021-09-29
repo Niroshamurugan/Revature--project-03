8fe04626eead7fb1e58bfd9b5df2369c1a8c9d2d
Nirosha Murugan <62345347+Niroshamurugan@users.noreply.github.com>
2021-09-29 13:47:22 +0530

    Add files via upload

--- code/producer.py
+++ code/producer.py
@@ -8,7 +8,7 @@ import credentials as cred
 key = cred.login['private_key']
 
 def send_Mumbai():
-    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Mumbai&country=IN&start_date=2021-09-15&end_date=2021-09-19&key={}'.format(key)
+    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Mumbai&country=IN&start_date=2021-09-24&end_date=2021-09-26&key={}'.format(key)
     data = requests.get(url).json()
     city_name = data['city_name']
 
@@ -18,12 +18,12 @@ def send_Mumbai():
         del dataval["timestamp_utc"]
         del dataval["timestamp_local"]
         output = json.dumps(dataval)
-        producer.send('crypto', output)
+        producer.send('weather', output)
         producer.flush()
         time.sleep(4)
 
 def send_Chennai():
-    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Chennai&country=IN&start_date=2021-09-15&end_date=2021-09-19&key={}'.format(key)
+    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Chennai&country=IN&start_date=2021-09-24&end_date=2021-09-26&key={}'.format(key)
     data = requests.get(url).json()
     city_name = data['city_name']
 
@@ -33,12 +33,12 @@ def send_Chennai():
         del dataval["timestamp_utc"]
         del dataval["timestamp_local"]
         output = json.dumps(dataval)
-        producer.send('crypto', output)
+        producer.send('weather', output)
         producer.flush()
         time.sleep(4)
 
 def send_Banglore():
-    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Banglore&country=IN&start_date=2021-09-15&end_date=2021-09-19&key={}'.format(key)
+    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Banglore&country=IN&start_date=2021-09-24&end_date=2021-09-26&key={}'.format(key)
     data = requests.get(url).json()
     city_name = data['city_name']
 
@@ -48,12 +48,12 @@ def send_Banglore():
         del dataval["timestamp_utc"]
         del dataval["timestamp_local"]
         output = json.dumps(dataval)
-        producer.send('crypto', output)
+        producer.send('weather', output)
         producer.flush()
         time.sleep(4)
 
 def send_Hyderabad():
-    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Hyderabad&country=IN&start_date=2021-09-15&end_date=2021-09-19&key={}'.format(key)
+    url = 'https://api.weatherbit.io/v2.0/history/subhourly?&city=Hyderabad&country=IN&start_date=2021-09-24&end_date=2021-09-26&key={}'.format(key)
     data = requests.get(url).json()
     city_name = data['city_name']
 
@@ -63,7 +63,7 @@ def send_Hyderabad():
         del dataval["timestamp_utc"]
         del dataval["timestamp_local"]
         output = json.dumps(dataval)
-        producer.send('crypto', output)
+        producer.send('weather', output)
         producer.flush()
         time.sleep(4)
 
